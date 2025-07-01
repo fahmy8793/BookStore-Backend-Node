19efe5aa9b4f139c3d4fedd6de5eaed249c10779
Mohamed Ramadan <mhmdrmdan652@gmail.com>
2025-07-01 21:10:30 +0300

    isAdmin middleware

---
+++ middlewares/isAdmin.js
@@ -0,0 +1,10 @@
+// use it in books routes <<CRUD>> after authmiddleware
+const isAdmin = async (req, res, next) => {
+    if (req.user && req.user.role == "admin") {
+        next();
+    } else {
+        return res.status(403).json({ message: 'Access denied' });
+    }
+}
+
+module.exports = isAdmin;
